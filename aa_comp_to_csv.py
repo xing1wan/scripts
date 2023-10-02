eb71f6dbb0014752c5d9f32609183103091b3746
Meggie <142974787+xing1wan@users.noreply.github.com>
2023-10-02 10:37:48 +0300

    aa composition calculator
    aa composition calculation v2
    Update: allow the output format to csv directly
    the old aa _composition_X.py is deprecated

---
+++ aa_comp_to_csv.py
@@ -0,0 +1,59 @@
+import sys
+import csv
+from Bio import SeqIO
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
+
+valid_amino_acids = set('ACDEFGHIKLMNPQRSTVWY')  # Standard amino acids
+
+if len(sys.argv) != 3:
+    print("Usage: python3 aa_composition.py input.fasta output.csv")
+    sys.exit(1)
+
+input_fasta_file = sys.argv[1]
+output_file = sys.argv[2]
+results = []
+
+try:
+    with open(input_fasta_file, 'r') as fasta_file:
+        for record in SeqIO.parse(fasta_file, 'fasta'):
+            protein_sequence = ''.join(aa for aa in str(record.seq) if aa in valid_amino_acids)
+            
+            if not protein_sequence:
+                continue
+            
+            analysis = ProteinAnalysis(protein_sequence)
+            amino_acid_composition = analysis.get_amino_acids_percent()
+            count_amino_acids = analysis.count_amino_acids()
+            
+            results.append({
+                'id': record.id,
+                'molecular_weight': analysis.molecular_weight(),
+                'isoelectric_point': analysis.isoelectric_point(),
+                'number_of_acidic_amino_acids': count_amino_acids['D'] + count_amino_acids['E'],
+                'number_of_basic_amino_acids': count_amino_acids['R'] + count_amino_acids['H'] + count_amino_acids['K'],
+                'frequency_of_r_group_positively_charged_acids': sum(amino_acid_composition[aa] for aa in 'KRH'),
+                'frequency_of_r_group_negatively_charged_acids': sum(amino_acid_composition[aa] for aa in 'DE'),
+                'frequency_of_polar_r_group': sum(amino_acid_composition[aa] for aa in 'STCPNQ'),
+                'frequency_of_nonpolar_aliphilic_r_group': sum(amino_acid_composition[aa] for aa in 'GAVLMI'),
+                'frequency_of_nonpolar_aromatic_r_group': sum(amino_acid_composition[aa] for aa in 'FYW'),
+                **{f'{aa.lower()}_freq': amino_acid_composition.get(aa, 0) for aa in valid_amino_acids}  # Include individual amino acid compositions
+            })
+
+except FileNotFoundError:
+    sys.exit(f"Error: {input_fasta_file} not found.")
+except Exception as e:
+    sys.exit(f"Error: {str(e)}")
+
+# Write results for each protein to the output CSV file
+try:
+    with open(output_file, 'w', newline='') as out_file:
+        fieldnames = ['id', 'molecular_weight', 'isoelectric_point', 'number_of_acidic_amino_acids', 'number_of_basic_amino_acids', 
+                      'frequency_of_r_group_positively_charged_acids', 'frequency_of_r_group_negatively_charged_acids', 
+                      'frequency_of_polar_r_group', 'frequency_of_nonpolar_aliphilic_r_group', 'frequency_of_nonpolar_aromatic_r_group'] \
+                    + [f'{aa.lower()}_freq' for aa in valid_amino_acids]
+        writer = csv.DictWriter(out_file, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        writer.writerows(results)
+except Exception as e:
+    sys.exit(f"Error writing to {output_file}: {str(e)}")
